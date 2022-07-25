de0e2c0cc6128143321fbd6f04d4526af3a77d0a
ibraim <ikoraikir@gmail.com>
2022-07-25 08:11:43 -0700

    add home pag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './componets/nav/Nav';
-
+import Home from './componets/nav/home/Home';
 function App() {
   return (
     <div className="App">
       
       <Nav/>
+     <Home/>
     </div>
   );
 }
--- src/componets/nav/home/Home.js
+++ src/componets/nav/home/Home.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import {BsFacebook ,BsGithub , BsTwitter} from "react-icons/bs"
+import "./home.css"
+import img from "../../../11.png"
 
 export default function Home() {
+
   return (
     <div className='home'>
      <div className='icon'>
-        
+       <a><BsFacebook size={25}/></a>
+       <a><BsGithub size={25}/></a>
+       <a>< BsTwitter size={25}/></a>
      </div>
-     <div className='about'></div>
-     <div className='img'></div>
+     <div className='img'>
+        <img src={img}/>
+     </div>
+     <div className='about'>
+      <h1> hello iam <span>ibrahim</span> </h1>
+      <h3> iam front-end web dve</h3>
+    
+      <p>Lorem ipsum dolor sit amet cons <br/> adipisicing elit. Minima, consectetur <br/> ullam?</p>
+            <button class="btn">Contact Me</button>
+      </div>
     </div>
   )
 }
